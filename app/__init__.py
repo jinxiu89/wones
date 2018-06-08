ccf2ab93b576c7ddc7bde4292c1ce8fed77898ac
thomas <jinxiu89@163.com>
2018-06-08 21:36:54 +0800

    edit markdown

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,6 @@ from .main import main
 
 def create_app(config_name):
     app = Flask(__name__)
-    # 配置加载
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
     app.register_blueprint(admin)
